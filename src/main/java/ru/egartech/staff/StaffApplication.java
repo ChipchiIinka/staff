f32ac7227c6565d97fecd3a7bec8a520a08ed63f
ChipchiIinka <m.gi2016@yandex.ru>
2024-08-22 20:50:44 +0300

    #4 fix dockerfile and docker-compose, some changes in openapi files

--- src/main/java/ru/egartech/staff/StaffApplication.java
+++ src/main/java/ru/egartech/staff/StaffApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StaffApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StaffApplication.class, args);
     }
-
 }
